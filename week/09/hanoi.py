846eb1efb7aee0a3aa70157ff1a7f5f1e0c6f3e5
William Bryce <bryce2@illinois.edu>
2017-10-19 11:50:13 -0500

    modified hanoi.py

--- week/09/hanoi.py
+++ week/09/hanoi.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 class Towers:
-
+    """ Towers class with three towers for solving 
+    Towers of Hanoi
+    """
     def __init__(self, num_disks):
         self.num_disks = num_disks
         self.start = []
@@ -13,21 +15,24 @@ class Towers:
     def show(self):
         print('{}\n{}\n{}\n'.format(self.start, self.middle, self.goal))
 
-    def move(self, n, source, aux, target):
-        """ Recursive method for solving Towers of Hanoi 
-        Taken from https://www.python-course.eu/towers_of_hanoi.php"""
-        if n > 0:
-            # switch target depending on odd or even # of disks
-            self.move(n-1, source, target, aux)
-            # source and target established, make a move
-            if len(source) > 0:
-                target.append(source.pop())
-                self.show()
-            # switch source depending on odd or even # of disks
-            self.move(n-1, aux, source, target)
+
+def move(n, source, aux, target):
+    """ Recursive method for solving Towers of Hanoi 
+    """ 
+    if n > 0:
+        # switch target depending on odd or even # of disks
+        move(n-1, source, target, aux)
+        # source and target established, make a move
+        if len(source) > 0:
+            target.append(source.pop())
+            towers.show() #--> possible because of global scope of variable
+        # switch source depending on odd or even # of disks
+        move(n-1, aux, source, target)
 
 
 def play_towers_of_hanoi():
+    """ Instantiates and solves Towers of Hanoi 
+    """
     while True:
         num_disks = input("Enter the number of disks: ")
         if num_disks.isdigit():
@@ -36,9 +41,11 @@ def play_towers_of_hanoi():
         else:
             print("Try again. Please enter an integer value.")
 
+    global towers # 'global' make variable visible outside of this method
     towers = Towers(num_disks)
     towers.show()
-    towers.move(len(towers.start), towers.start, towers.middle, towers.goal) 
+    move(len(towers.start), towers.start, towers.middle, towers.goal) 
+
 
 if __name__ == '__main__':
     play_towers_of_hanoi()
